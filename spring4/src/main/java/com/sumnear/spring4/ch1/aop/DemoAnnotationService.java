c58f1025233b5112f554f622eac2c1e852d44264
Sum Near <402347012@qq.com>
2019-05-19 22:32:51 +0800

    我的天。

--- spring4/src/main/java/com/sumnear/spring4/ch1/aop/DemoAnnotationService.java
+++ spring4/src/main/java/com/sumnear/spring4/ch1/aop/DemoAnnotationService.java
@@ -2,13 +2,17 @@ package com.sumnear.spring4.ch1.aop;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * @author sumnear
+ */
+
 @Service
 public class DemoAnnotationService
 {
+
     @Action(name = "注解式拦截的add操作")
     public void add()
     {
         System.out.println("DemoAnnotationService  add");
     }
-
 }
