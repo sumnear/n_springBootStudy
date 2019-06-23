948f7b3b8698f34a2ac7fd43914ea642159cb007
Sum Near <402347012@qq.com>
2019-06-23 22:41:28 +0800

    springboot ch7&11

---
+++ springboot_ch11/src/main/java/com/example/springboot_ch11/StatusService.java
@@ -0,0 +1,19 @@
+package com.example.springboot_ch11;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatusService {
+	
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+}
